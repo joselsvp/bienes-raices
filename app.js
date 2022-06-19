87fced2078a284f7cf4684f627044923b2b6ea27
Jose Villanueva <lv428694@gmail.com>
2022-06-18 22:17:58 -0500

    add: get routing

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require('express')
 //create app
 const app = express()
 
+//Routing
+app.get('/', function(request, response) {
+    response.send('Hola mundo en express')
+});
+
 
 //Define Port and start project
 const port = 3000;
