c1afb0f84028da1e91e3e755d9355dec977b31f2
Jose Villanueva <lv428694@gmail.com>
2022-06-18 22:08:21 -0500

    add: start run project express last version
    run script in console nodemono: npm run server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("test dev node js");
+//last vension to import express
+const express = require('express')
+
+//create app
+const app = express()
+
+
+//Define Port and start project
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running in port 3000`);
+});
